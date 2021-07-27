95a3d1540d40ed921994e18e345f35341286433d
Paul Gerads <pdgerads@gmail.com>
2021-07-27 02:57:38 -0500

    feat(1.3): using Harmony to bypass the natural goodwill check

---
+++ Source/RemoveGradualRelationsDecay/HarmonyPatches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using Verse;
+
+namespace RemoveGradualRelationsDecay
+{
+	[StaticConstructorOnStartup]
+    public class HarmonyPatches
+	{
+		private static readonly Type patchType = typeof(HarmonyPatches);
+
+		static HarmonyPatches()
+		{
+			var harmony = new Harmony(id: "rimworld.gerads.removegradualrelationsdecay");
+
+			harmony.Patch(AccessTools.Method(typeof(void), "CheckReachNaturalGoodwill"), prefix: new HarmonyMethod(patchType, "CheckReachNaturalGoodwillPrefix"));
+		}
+
+		public static bool CheckReachNaturalGoodwillPrefix()
+		{
+			return false; // TODO: set up settings
+		}
+	}
+}
